c90beb0cefecf87fc1be1504d3002b8072a645c2
Adam Gordon <adamjgordon24@gmail.com>
2019-01-25 02:43:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,8 @@
 def speak_to_grandma(string)
   if string.upcase != string
     "HUH?! SPEAK UP, SONNY!"
-    elsif string == 
+  elsif string == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else 
+    "NO, NOT SINCE 1938!"
 end
